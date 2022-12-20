e76a61cd9c7da2ec5113d2654eb3f18d030d4e31
ashtiv <ashishtiwari05032002@gmail.com>
2022-12-20 20:07:42 +0530

    just some body elemets completed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <div className='body-1'>
+        <div className='body-2'>
+          <div className='body-3'>
+            <div className='body-box1'>
+              <div className='body-box-number'>99.95%</div>
+              <div className='body-box-terms'>Accuracy rate</div>
+              <div className='body-box-description'>in fulfilling orders</div>
+            </div>
+            <div className='body-box2'>
+              <div className='body-box-number'>5000+</div>
+              <div className='body-box-terms'>Ecommerce businesses</div>
+              <div className='body-box-description'>partner with Opus</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
